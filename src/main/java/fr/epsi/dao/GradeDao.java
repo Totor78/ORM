4c2a0d553ca0ec55977fe08b8e0ede7400910e1f
ROBIN Alexandre <arobin@yellownetwork.fr>
2019-03-18 14:59:37 +0100

    update

---
+++ src/main/java/fr/epsi/dao/GradeDao.java
@@ -0,0 +1,7 @@
+package fr.epsi.dao;
+
+import fr.epsi.model.Grade;
+
+public class GradeDao extends AbstractDao<Grade> {
+
+}
---
+++ src/main/java/fr/epsi/dao/TeacherDao.java
@@ -0,0 +1,7 @@
+package fr.epsi.dao;
+
+import fr.epsi.model.Teacher;
+
+public class TeacherDao extends AbstractDao<Teacher> {
+
+}
