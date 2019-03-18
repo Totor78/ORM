6608f044878078587b215bd5a7ee6e618fb761b6
ROBIN Alexandre <arobin@yellownetwork.fr>
2019-03-18 14:39:24 +0100

    add class

--- src/main/java/fr/epsi/model/Teacher.java
+++ src/main/java/fr/epsi/model/Teacher.java
@@ -1,4 +1,38 @@
 package fr.epsi.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Teacher {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstname;
+    private String lastname;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
 }
